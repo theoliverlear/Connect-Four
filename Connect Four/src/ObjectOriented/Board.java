0510d69d5d15a6bd084dd6d04d4d624a26da8eb6
theoliverlear <sigwartholiver2@gmail.com>
2023-09-03 21:55:47 -0500

    Refactor and add comments

--- Connect Four/src/ObjectOriented/Board.java
+++ Connect Four/src/ObjectOriented/Board.java
@@ -1,7 +1,6 @@
 package ObjectOriented;
 
 import java.util.Arrays;
-
 public class Board {
     char[][] board;
     int rowSize;
@@ -11,7 +10,16 @@ public class Board {
         rowSize = board.length;
         colSize = board[0].length;
     }
-
+    //----------------------------Utility-Methods-----------------------------
+    public void resetBoard() {
+        for (int row = 0; row < this.getRowSize(); row++) {
+            Arrays.fill(this.board[row], ' ');
+        }
+    }
+    public boolean isTaken(int row, int col) {
+        return this.getCharAt(row, col) != ' ';
+    }
+    //------------------------------Getters-----------------------------------
     public int getRowSize() {
         return this.rowSize;
     }
@@ -21,20 +29,8 @@ public class Board {
     public char getCharAt(int row, int col) {
         return this.board[row][col];
     }
+    //------------------------------Setters-----------------------------------
     public void setBoardSpot(int row, int col, char currentPlayerPiece) {
         this.board[row][col] = currentPlayerPiece;
     }
-    public void resetBoard() {
-        for (int row = 0; row < this.getRowSize(); row++) {
-            Arrays.fill(this.board[row], ' ');
-        }
-    }
-    public boolean isEmpty(int row, int col) {
-        if (" ".equals(String.valueOf(this.getCharAt(row, col)))) {
-            return false;
-        } else {
-            return true;
-        }
-    }
-
 }
